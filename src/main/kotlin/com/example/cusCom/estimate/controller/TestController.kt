3974d112e4329d5f771975d02fa8afc17513ee35
han <hcsy1219@gmail.com>
2023-07-03 16:36:51 +0900

    feat: Add custom page prototype

--- src/main/kotlin/com/example/cusCom/estimate/controller/TestController.kt
+++ src/main/kotlin/com/example/cusCom/estimate/controller/TestController.kt
@@ -6,44 +6,45 @@ import com.example.cusCom.estimate.service.DesktopPartsService
 import com.example.cusCom.estimate.service.EstimateService
 import com.example.cusCom.estimate.service.parts.*
 import com.google.gson.Gson
+import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@Controller
 class TestController(private val desktopPartsService: DesktopPartsService,
                      private val estimateService: EstimateService) {
 
     @GetMapping("/test")
-    fun getDataTest(){
-        desktopPartsService.getCaseList()
-        desktopPartsService.getCpuCoolerList()
-        desktopPartsService.getCPUList()
-        desktopPartsService.getDataStorageList()
-        desktopPartsService.getGraphicsCardList()
-        desktopPartsService.getMemoryList()
-        desktopPartsService.getMotherBoardList()
-        desktopPartsService.getPowerSupplyList()
+    fun getDataTest(model:Model):String{
+        model.addAttribute("caseList",desktopPartsService.getCaseList())
+        model.addAttribute("cpuCoolerList",desktopPartsService.getCpuCoolerList())
+        model.addAttribute("cpuList",desktopPartsService.getCPUList())
+        model.addAttribute("dataStorageList",desktopPartsService.getDataStorageList())
+        model.addAttribute("graphicsCardList",desktopPartsService.getGraphicsCardList())
+        model.addAttribute("memoryList",desktopPartsService.getMemoryList())
+        model.addAttribute("motherBoardList",desktopPartsService.getMotherBoardList())
+        model.addAttribute("powerSupplyList",desktopPartsService.getPowerSupplyList())
+        return "customPage"
     }
 
     @PostMapping("/postTest")
     fun postDataTest(@RequestParam("estimate") estimateJSON:String):String{
         val tempEstimates = Gson().fromJson(estimateJSON,Estimate::class.java)
         estimateService.checkDesktopEstimate(tempEstimates)
-        return "clear"
+        return "clearView"
     }
 
-    @GetMapping("clear")
+    @GetMapping("/clear")
     fun testClearPage(){}
-    @GetMapping("fail")
+    @GetMapping("/fail")
     fun testFailPage(){}
 
     @ExceptionHandler(EstimateException::class)
     fun estimateException(ex:EstimateException,model:Model):String{
         model.addAttribute("errorMsg",ex.message)
-        return "fail"
+        return "failView"
     }
 }

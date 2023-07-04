0b89a5268b50526226e12d53835d09639001e5b0
han <hcsy1219@gmail.com>
2023-07-04 23:42:21 +0900

    feat: Add CRUD function to desktop parts service

--- src/main/kotlin/com/example/cusCom/estimate/service/DesktopPartsService.kt
+++ src/main/kotlin/com/example/cusCom/estimate/service/DesktopPartsService.kt
@@ -34,6 +34,24 @@ class DesktopPartsService(private val caseRepo: CaseRepository,
         return caseList
     }
 
+    @Transactional
+    fun createCase(case: Case): CaseEntity {
+        return caseRepo.save(case.toCaseEntity())
+    }
+
+    @Transactional
+    fun updateCase(case: Case): CaseEntity {
+        var temp=caseRepo.findById(case.name).get()
+        temp=case.toCaseEntity()
+        return temp
+    }
+
+    @Transactional
+    fun deleteCase(case: Case){
+        return caseRepo.deleteById(case.name)
+    }
+
+
     @Transactional(readOnly = true)
     fun getCpuCoolerList(): List<CPUCooler> {
         val cpuCoolerList:List<CPUCooler> = cpuCoolerRepo.findAll().map{
@@ -49,6 +67,24 @@ class DesktopPartsService(private val caseRepo: CaseRepository,
         }
         return cpuCoolerList
     }
+
+    @Transactional
+    fun createCPUCooler(cpuCooler: CPUCooler): CPUCoolerEntity {
+        return cpuCoolerRepo.save(cpuCooler.toCPUCoolerEntity())
+    }
+
+    @Transactional
+    fun updateCPUCooler(cpuCooler: CPUCooler): CPUCoolerEntity {
+        var temp=cpuCoolerRepo.findById(cpuCooler.name).get()
+        temp=cpuCooler.toCPUCoolerEntity()
+        return temp
+    }
+
+    @Transactional
+    fun deleteCPUCooler(cpuCooler: CPUCooler){
+        return cpuCoolerRepo.deleteById(cpuCooler.name)
+    }
+
 
     @Transactional(readOnly = true)
     fun getCPUList(): List<CPU> {
@@ -67,6 +103,24 @@ class DesktopPartsService(private val caseRepo: CaseRepository,
         return cpuList
     }
 
+    @Transactional
+    fun createCPU(cpu: CPU): CPUEntity {
+        return cpuRepo.save(cpu.toCPUEntity())
+    }
+
+    @Transactional
+    fun updateCPU(cpu: CPU): CPUEntity {
+        var temp=cpuRepo.findById(cpu.name).get()
+        temp=cpu.toCPUEntity()
+        return temp
+    }
+
+    @Transactional
+    fun deleteCPU(cpu: CPU){
+        return cpuRepo.deleteById(cpu.name)
+    }
+
+
     @Transactional(readOnly = true)
     fun getDataStorageList(): List<DataStorage> {
         val dataStorageList:List<DataStorage> = dataStorageRepo.findAll().map{
@@ -81,6 +135,24 @@ class DesktopPartsService(private val caseRepo: CaseRepository,
         }
         return dataStorageList
     }
+
+    @Transactional
+    fun createDataStorage(dataStorage: DataStorage): DataStorageEntity {
+        return dataStorageRepo.save(dataStorage.toDataStorageEntity())
+    }
+
+    @Transactional
+    fun updateDataStorage(dataStorage: DataStorage): DataStorageEntity {
+        var temp=dataStorageRepo.findById(dataStorage.name).get()
+        temp=dataStorage.toDataStorageEntity()
+        return temp
+    }
+
+    @Transactional
+    fun deleteDataStorage(dataStorage: DataStorage){
+        return dataStorageRepo.deleteById(dataStorage.name)
+    }
+
 
     @Transactional(readOnly = true)
     fun getGraphicsCardList(): List<GraphicsCard> {
@@ -97,6 +169,24 @@ class DesktopPartsService(private val caseRepo: CaseRepository,
         return graphicsCardList
     }
 
+    @Transactional
+    fun createGraphicsCard(graphicsCard: GraphicsCard): GraphicsCardEntity {
+        return graphicsCardRepo.save(graphicsCard.toGraphicsCardEntity())
+    }
+
+    @Transactional
+    fun updateGraphicsCard(graphicsCard: GraphicsCard): GraphicsCardEntity {
+        var temp=graphicsCardRepo.findById(graphicsCard.name).get()
+        temp=graphicsCard.toGraphicsCardEntity()
+        return temp
+    }
+
+    @Transactional
+    fun deleteGraphicsCard(graphicsCard: GraphicsCard){
+        return graphicsCardRepo.deleteById(graphicsCard.name)
+    }
+
+
     @Transactional(readOnly = true)
     fun getMemoryList(): List<Memory> {
         val memoryList:List<Memory> = memoryRepo.findAll().map{
@@ -108,6 +198,24 @@ class DesktopPartsService(private val caseRepo: CaseRepository,
         }
         return memoryList
     }
+
+    @Transactional
+    fun createMemory(memory: Memory): MemoryEntity {
+        return memoryRepo.save(memory.toMemoryEntity())
+    }
+
+    @Transactional
+    fun updateMemory(memory: Memory): MemoryEntity {
+        var temp=memoryRepo.findById(memory.name).get()
+        temp=memory.toMemoryEntity()
+        return temp
+    }
+
+    @Transactional
+    fun deleteMemory(memory: Memory){
+        return memoryRepo.deleteById(memory.name)
+    }
+
 
     @Transactional(readOnly = true)
     fun getMotherBoardList(): List<MotherBoard> {
@@ -126,6 +234,24 @@ class DesktopPartsService(private val caseRepo: CaseRepository,
         return motherBoardList
     }
 
+    @Transactional
+    fun createMotherBoard(motherBoard: MotherBoard): MotherBoardEntity {
+        return motherBoardRepo.save(motherBoard.toMotherBoardEntity())
+    }
+
+    @Transactional
+    fun updateMotherBoard(motherBoard: MotherBoard): MotherBoardEntity {
+        var temp=motherBoardRepo.findById(motherBoard.name).get()
+        temp=motherBoard.toMotherBoardEntity()
+        return temp
+    }
+
+    @Transactional
+    fun deleteMotherBoard(motherBoard: MotherBoard){
+        return motherBoardRepo.deleteById(motherBoard.name)
+    }
+
+
     @Transactional(readOnly = true)
     fun getPowerSupplyList(): List<PowerSupply> {
         val powerSupplyList:List<PowerSupply> = powerSupplyRepo.findAll().map{
@@ -136,5 +262,22 @@ class DesktopPartsService(private val caseRepo: CaseRepository,
             entity.modular,
             entity.length)}
         return powerSupplyList
+    }
+
+    @Transactional
+    fun createPowerSupply(powerSupply: PowerSupply): PowerSupplyEntity {
+        return powerSupplyRepo.save(powerSupply.toPowerSupplyEntity())
+    }
+
+    @Transactional
+    fun updatePowerSupply(powerSupply: PowerSupply): PowerSupplyEntity {
+        var temp=powerSupplyRepo.findById(powerSupply.name).get()
+        temp=powerSupply.toPowerSupplyEntity()
+        return temp
+    }
+
+    @Transactional
+    fun deletePowerSupply(powerSupply: PowerSupply){
+        return powerSupplyRepo.deleteById(powerSupply.name)
     }
 }

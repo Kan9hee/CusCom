8104e7f75f2dd6fbb2be74f4cdcba3437ee85536
han <hcsy1219@gmail.com>
2023-07-06 19:31:20 +0900

    feat: Add data management page

--- src/main/kotlin/com/example/cusCom/estimate/controller/AdminController.kt
+++ src/main/kotlin/com/example/cusCom/estimate/controller/AdminController.kt
@@ -1,21 +1,21 @@
 package com.example.cusCom.estimate.controller
 
-import com.example.cusCom.estimate.dto.Estimate
-import com.example.cusCom.estimate.exception.EstimateException
+import com.example.cusCom.estimate.dto.parts.CPU
 import com.example.cusCom.estimate.service.DesktopPartsService
-import com.example.cusCom.estimate.service.EstimateService
-import com.example.cusCom.estimate.service.parts.*
 import com.google.gson.Gson
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
-class TestController(private val desktopPartsService: DesktopPartsService,
-                     private val estimateService: EstimateService) {
+@RequestMapping("/adminPage")
+class AdminController(private val desktopPartsService: DesktopPartsService) {
 
-    @GetMapping("/test")
-    fun getDataTest(model:Model):String{
+    @GetMapping("/main")
+    fun mainPage(model: Model):String{
         model.addAttribute("caseList",desktopPartsService.getCaseList())
         model.addAttribute("cpuCoolerList",desktopPartsService.getCpuCoolerList())
         model.addAttribute("cpuList",desktopPartsService.getCPUList())
@@ -24,17 +24,17 @@ class TestController(private val desktopPartsService: DesktopPartsService,
         model.addAttribute("memoryList",desktopPartsService.getMemoryList())
         model.addAttribute("motherBoardList",desktopPartsService.getMotherBoardList())
         model.addAttribute("powerSupplyList",desktopPartsService.getPowerSupplyList())
-        return "customPage"
+        return "adminMainPage"
     }
 
-    @GetMapping("/clear")
-    fun testClearPage(){}
-    @GetMapping("/fail")
-    fun testFailPage(){}
+    @GetMapping("/editCPU")
+    fun dataTest():String{
+        return "dataInputPage"
+    }
 
-    @ExceptionHandler(EstimateException::class)
-    fun estimateException(ex:EstimateException,model:Model):String{
-        model.addAttribute("errorMsg",ex.message)
-        return "failView"
+    @PostMapping("/editCPU")
+    fun postDataTest(@RequestParam("CPU") cpuJSON:String):String{
+        desktopPartsService.createCPU(Gson().fromJson(cpuJSON, CPU::class.java))
+        return "redirect:/main"
     }
 }

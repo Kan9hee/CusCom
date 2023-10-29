faea9e1a80f2c273032222cf14a36e7dfec85d76
han <hcsy1219@gmail.com>
2023-10-29 17:09:06 +0900

    fix: Fix membership join function error

--- src/main/kotlin/com/example/cusCom/config/SecurityConfig.kt
+++ src/main/kotlin/com/example/cusCom/config/SecurityConfig.kt
@@ -14,7 +14,7 @@ class SecurityConfig {
     fun filterChain(http:HttpSecurity):SecurityFilterChain{
         http.csrf{ it.disable() }
             .authorizeHttpRequests{ authorize -> authorize
-                .requestMatchers("/CusCom/joinPage","/CusCom/join","/CusCom/loginPage").anonymous()
+                .requestMatchers("/CusCom/joinPage","/CusCom/API/join","/CusCom/loginPage").anonymous()
                 .requestMatchers("/CusCom/adminPage/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
             }

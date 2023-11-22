635ae4f608b48508e7f0c27c71e4e69adb478543
Kanghee Han <hcsy1219@gmail.com>
2023-11-22 23:43:02 +0900

    fix: Fix external data error due to snake case

--- src/main/kotlin/com/example/cusCom/config/DBStringConfig.kt
+++ src/main/kotlin/com/example/cusCom/config/DBStringConfig.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@ConfigurationProperties(prefix = "dbString")
+@ConfigurationProperties(prefix = "db-string")
 class DBStringConfig {
     lateinit var mysql: MysqlProperties
     lateinit var mongodb: MongodbProperties
